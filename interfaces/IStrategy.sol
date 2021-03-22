595afe3c6669f08e88b26c4d28f7619793715788
marco mariscal <marco.a.mariscal@gmail.com>
2021-03-21 21:18:13 -0400

    adding contracts and interfaces

---
+++ interfaces/IStrategy.sol
@@ -0,0 +1,21 @@
+pragma solidity >=0.6.0;
+
+interface IStrategy {
+    // the native token
+    function want() external view returns (address);
+
+    // Deposits token (same as want() returns) into contract specified by the Strategy (might not need to use this if the strategy requires arb/trading methodology).
+    function deposit() external;
+
+    // dust collecting function that returns tokens to the vault if incorrectly sent to strategy; withdraw here should return to the Controller
+    function withdraw(address) external;
+
+    // partially withdraw funds denominated in want() token from the strategy to the appropriate vault; only controller should be able to call this
+    function withdraw(uint256) external;
+
+    // withdraw should return to the Vault; typically used when migrating/changing strategies
+    function withdrawAll() external returns (uint256);
+
+    // current want() balance in the strategy
+    function balanceOf() external view returns (uint256);
+}
---
+++ interfaces/IVault.sol
@@ -0,0 +1,30 @@
+pragma solidity >=0.5.0;
+
+interface IVault {
+    function token() external view returns (address);
+
+    function name() external view returns (string memory);
+
+    function symbol() external view returns (string memory);
+
+    function decimals() external view returns (uint8);
+
+    function controller() external view returns (address);
+
+    function governance() external view returns (address);
+
+    // native token balance (e.g.: DAI) (current balance in the vault, controller, and strategy contracts) divided by vault token (xlDAI) total supply
+    function getPricePerFullShare() external view returns (uint256);
+
+    // deposits into the vault
+    function deposit(uint256) external;
+
+    // deposits maximum amount of native token into the vault
+    function depositAll() external;
+
+    // withdraws native token from the vault
+    function withdraw(uint256) external;
+
+    // withdraws maximum amount of native token from the vault
+    function withdrawAll() external;
+}
